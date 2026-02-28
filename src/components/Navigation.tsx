cd9fc0a76d7da25388a0ba49a466acea58f21a2a
shilvockadler7-tech <shilvockadler7@gmail.com>
2026-02-28 14:07:19 -0800

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -28,13 +28,13 @@ export default function Navigation() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-2">
+        <div className="flex justify-between items-center py-3">
           <div className="flex-shrink-0">
             <img
               src="/logo.png"
               alt="BinBuddy Logo"
               className="w-auto"
-              style={{ height: '80px' }}
+              style={{ height: '120px' }}
             />
           </div>
 
